9818f27e143d5f6a65232b88e16e7ae64d53ab80
anoint <dbwlsk870@hanmail.net>
2019-10-30 18:58:01 +0900

    20191030

--- js200제/따라친문법/Object.js
+++ js200제/따라친문법/Object.js
@@ -1,8 +1,27 @@
 /*
 객체 문법 익히기
-- 가족 객체를 선언하고 
+- 가족 객체를 선언한다.
   키를 4개를 추가한다. 
-  주소와 구성원과 가족을 추가하는 함수와 구성원의 인원수르 출력하는 함수 
+  주소와 구성원과 가족의 나이와 이름을 추가하는 함수와 구성원의 인원수르 출력하는 함수 
   가족 객체에 구성원을 3명 추가한다.
   인원수를 출력하는 함수를 호출하여 출력한다.
-  */
+*/
+var family = {
+    'address' : 'Seoul',
+    members : {},
+    addFamily : function(name, age, role) {
+        this.members[role] = {
+            name : name,
+            age : age
+        }
+    },
+    getHeadCount: function() {
+        return Object.keys(this.members).length;
+    }
+};
+
+family.addFamily(30, 'chole', 'aunt');
+family.addFamily(30, 'lyn', 'niece');
+family.addFamily(30, 'jina', 'humen');
+
+console.log(family.getHeadCount());
--- js200제/따라친문법/bit.js
+++ js200제/따라친문법/bit.js
@@ -1,19 +1,20 @@
-console.log(14*11);
-console.log(~14);
-console.log(14|11);
-console.log(14^11);
-console.log(2<<2);
-console.log(14>>1);
-console.log(14>>>2)
+// console.log(14*11);
+// console.log(~14);
+// console.log(14|11);
+// console.log(14^11);
+// console.log(2<<2);
+// console.log(14>>1);
+// console.log(14>>>2)
 
 var num = 14, num2 = 11;
-var oper = ['&','~'];
-var result = `${num} ${oper[0]} ${num2}`;
-num = num.toString(2);
-num2 = num2.toString(2);
-console.log(`${num}와 ${num2}의 논리곱& 연산 결과는? ${result}`);
-console.log(num2);
-console.log(num * num2);
-console.log(~num);
+var oper = ['&','|','^','<<','>>','>>>'];
+oper.forEach(element => {
+    var result = `${num} ${element} ${num2}`;
+    // console.log(element);
+    console.log(`${num.toString(2)}와 ${num2.toString(2)}의 논리곱& 연산 결과는? ${eval(result)}`);
+});
+// console.log(num2);
+// console.log(num * num2);
+// console.log(~num);
 
 

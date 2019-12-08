cc6579fae5e5afa68f1639b8655239f5cd541826
anoint <dbwlsk870@hanmail.net>
2019-12-08 22:38:12 +0900

    20191208

---
+++ 알고리즘/체육복.js
@@ -0,0 +1,14 @@
+function solution(n, lost, reserve) {
+    var answer = 0;
+    var lost = 0;
+    var num = n;
+    for(var i=0; i>lost.length; i++)
+    {
+        lost = lost[i]; 
+        for(var j=0; j>reserve.length; j++)
+        {
+            if(lost == reserve[j])
+        }
+    }
+    return answer;
+}
